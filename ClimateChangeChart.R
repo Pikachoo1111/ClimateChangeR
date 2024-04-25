b56d3cb68f7e4e5d9778fdb2b09548ad2128cddf
Pikachoo1111 <104648868+Pikachoo1111@users.noreply.github.com>
2024-04-25 13:07:31 -0500

    Add files via upload

---
+++ ClimateChangeChart.R
@@ -0,0 +1,83 @@
+# title: Map any region of the world
+# author: FelixAnalytix.com
+
+
+## ----Install and load R packages-----------------
+if (!require("tidyverse")) install.packages("tidyverse")
+if (!require("rnaturalearth")) install.packages("rnaturalearth")
+if (!require("sf")) install.packages("sf")
+if (!require("wbstats")) install.packages("wbstats")
+
+library(tidyverse)
+library(rnaturalearth) # World Map Data from Natural Earth
+library(sf) # Geographic Simple Features in R
+library(wbstats) # access World Bank API
+
+
+## ----Download world data-------------------------
+world <- ne_countries(scale="medium", returnclass="sf") %>%
+  filter(admin != "Antarctica")
+
+
+
+target_crs <- "+proj=moll"
+
+world_moll <- world %>%
+  st_transform(crs = target_crs)
+
+
+# https://data.worldbank.org/indicator/EN.ATM.CO2E.PC
+ind <- "EN.ATM.CO2E.PC"
+
+indicator_info <- wb_cachelist$indicators %>%
+  filter(indicator_id == ind)
+
+indicator_info$indicator
+
+
+df <- wb_data(ind, start_date = 2020) %>%
+  filter(date == 2020)
+
+glimpse(df)
+
+
+df %>%
+  ggplot() +
+  geom_histogram(aes(EN.ATM.CO2E.PC)) +
+  theme_minimal() +
+  scale_x_sqrt()
+
+
+world_moll %>%
+  left_join(df, by = c("iso_a3" = "iso3c")) %>%
+  ggplot() +
+  geom_sf(aes(fill = EN.ATM.CO2E.PC)) +
+  scale_fill_viridis_c(
+    trans = "sqrt",
+    labels = scales::percent_format(scale = 1),
+    breaks = c(1:5)^2) +
+  # fix labels if needed: https://stackoverflow.com/a/60733863
+  scale_x_continuous(labels = function(x) paste0(x, '\u00B0', "W")) +
+  scale_y_continuous(labels = function(x) paste0(x, '\u00B0', "N")) +
+  theme_bw() +
+  theme(panel.background = element_rect(fill = "aliceblue")) +
+  labs(
+    title = paste(unique(df$date), indicator_info$indicator),
+    fill = NULL,
+  )
+
+
+## ----Create bounding box-------------------------
+# Choose manually the region to plot with Open Street Map
+# https://www.openstreetmap.org/export
+window_coord <- st_sfc(
+  st_point(c(-18, 32.5)), #left, bottom
+  st_point(c(40.4, 72.3)), #right, top
+  crs = 4326 #the EPSG identifier of WGS84 (used in GPS)
+)
+
+window_coord_sf <- window_coord %>%
+  st_transform(crs = target_crs) %>%
+  st_coordinates() # retrieve coordinates
+
+
